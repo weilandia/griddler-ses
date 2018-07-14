ba954fe77eb8e32284a32a89ddad1bd1812067ba
Nick Weiland <nickweiland@gmail.com>
2018-07-13 23:11:18 -0700

    Remove deprecated reference to ActionDispatch::ParamsParser

--- lib/griddler/ses/railtie.rb
+++ lib/griddler/ses/railtie.rb
@@ -2,7 +2,7 @@ module Griddler
   module Ses
     class Railtie < Rails::Railtie
       initializer "griddler_ses.configure_rails_initialization" do |app|
-        Rails.application.middleware.insert_before ActionDispatch::ParamsParser, Griddler::Ses::Middleware
+        Rails.application.middleware.use(Griddler::Ses::Middleware)
       end
     end
   end
